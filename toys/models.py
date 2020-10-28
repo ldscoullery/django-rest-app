3351d2e20244ad66622623bb63958abbfebf28fe
Leandro Saavedra <leandro.coullery@mercadolibre.com>
2020-10-27 21:17:55 -0300

    using ApiView class

--- toys/models.py
+++ toys/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Toy(models.Model):
-    created = models.DateTimeField(auto_now_add=True) 
+    created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=150, blank=False, default='')
     description = models.CharField(max_length=250, blank=True, default='')
     toy_category = models.CharField(max_length=200, blank=False, default='')
